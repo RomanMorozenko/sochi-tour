174b09db5ff05a292975b60cc3517cb348d4b736
RomanMorozenko <101452002+RomanMorozenko@users.noreply.github.com>
2023-12-11 21:18:11 +0700

    apiKey

--- src/utils/firebase-config.ts
+++ src/utils/firebase-config.ts
@@ -2,7 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyCQwrPAPd4aJkFaH4Y8d4wK6Iai-Ev8jNQ',
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
